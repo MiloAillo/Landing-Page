50538054c64de451b1c0ff97d3273c38a1424835
Mischiko Moe <panuwucake@gmail.com>
2025-07-07 09:40:35 +0700

    implementing parallax using framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { motion, useScroll, useTransform } from 'motion/react'
 import './App.css'
 
 function App() {
+const { scrollYProgress } = useScroll({
+  offset: ["start start", "end end"]
+})
+const card = useTransform(scrollYProgress, [0, 1], ["0vh", "50vh"])
+const layer1 = useTransform(scrollYProgress, [0, 1], ["0vh", "0vh"])
+const layer2 = useTransform(scrollYProgress, [0, 1], ["0vh", "5vh"])
+const layer3 = useTransform(scrollYProgress, [0, 1], ["0vh", "7vh"])
 
   return (
     <div>
       <div className='page1'>
-        <div className='page1 main-content'>
+        <motion.div className='page1 main-content' style={{y: card}}>
           <div className='content1 card'>
             <div className='pfp'></div>
             <div className='card-content'>
@@ -36,11 +44,11 @@ function App() {
               </div>
             </div>
           </div>
-        </div>
+        </motion.div>
+        <motion.div className='wave' id='wave1' style={{y: layer1}}></motion.div>
+        <motion.div className='wave' id='wave2' style={{y: layer2}}></motion.div>
+        <motion.div className='wave' id='wave3'style={{y: layer3}}></motion.div>
       </div>
-      <div className='wave' id='wave1'></div>
-      <div className='wave' id='wave2'></div>
-      <div className='wave' id='wave3'></div>
     </div>
   )
 }
