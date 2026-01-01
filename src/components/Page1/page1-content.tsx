88c5e68cd8e27d45b2aac0a2956c52ad54679d87
mischikomoe <panuwucake@gmail.com>
2026-01-01 22:32:46 +0700

    cleaning visual

--- src/components/Page1/page1-content.tsx
+++ src/components/Page1/page1-content.tsx
@@ -1,6 +1,8 @@
 import { motion, MotionValue } from 'motion/react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import "./page1-content.css"
 import type { Ref } from 'react'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 interface Page1ContentInterface {
     card: MotionValue<string>
@@ -13,8 +15,8 @@ function Page1Content({card, ref}: Page1ContentInterface) {
           <div className='content1 card'>
             <div className='pfp'></div>
             <div className='card-content'>
-              <p className='card-number'>0912873465</p>
               <p className='card-name'>Mischiko Moe</p>
+              <p className='card-number'>ID: 14152089147085185</p>
             </div>
             <div className='card-barcode'></div>
           </div>
@@ -22,7 +24,7 @@ function Page1Content({card, ref}: Page1ContentInterface) {
             <div className='intro'>
               <div className='name'>
                 <p className='name-dialogue'>Hello, my name is</p>
-                <p className='name-name'>Mischiko Moe</p>
+                <p className='name-name'>Faris Kahlil Haidar</p>
               </div>
               <p className='intro-desc'>A software engineer aiming to be a fullstack developer.</p>
             </div>
@@ -36,6 +38,10 @@ function Page1Content({card, ref}: Page1ContentInterface) {
                 <div className='social-link'>
                   <div className='social-link-logo email'></div>
                   <p className='social-link-tittle'><a href='mailto:fariskahlilhaidar@gmail.com'>fariskahlilhaidar@gmail.com</a></p>
+                </div>
+                <div className='social-link'>
+                  <FontAwesomeIcon icon={faLinkedin} size='2x' className='social-link-logo linkedin' />
+                  <p className='social-link-tittle'><a href='https://www.linkedin.com/in/faris-kahlil-haidar-7bb35031b/'>Faris Kahlil Haidar</a></p>
                 </div>
               </div>
             </div>
